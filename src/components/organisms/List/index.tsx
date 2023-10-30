491f229c055d612b6c12949c8886ec97d22fd012
EricTorresAndrade <eric.torres@nautilus.team>
2023-10-29 19:32:08 -0600

    UX
    FlashList performanced

--- src/components/organisms/List/index.tsx
+++ src/components/organisms/List/index.tsx
@@ -1,5 +1,7 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useEffect, useRef } from 'react';
+import { useBlankAreaTracker } from '@shopify/flash-list';
 import { screen_height } from '@utils/functions';
+import { Logger } from '@services';
 import ListItem from './components/ListItem';
 import { StyledList } from './styles';
 
@@ -18,6 +20,9 @@ export const List: React.FC<ListProps> = ({
   horizontal,
   showsHorizontalScrollIndicator,
 }) => {
+  const ref = useRef(null);
+  const [blankAreaTrakerResult, onBlankArea] = useBlankAreaTracker(ref);
+
   const renderElement = useCallback(
     ({ item, index }: { item: any; index: number }): JSX.Element | null => {
       return (
@@ -27,16 +32,32 @@ export const List: React.FC<ListProps> = ({
     [],
   );
 
+  const onLoadListener = useCallback(
+    (info: { elapsedTimeInMs: number }): void => {
+      Logger.log('onLoadListener: ', { info });
+    },
+    [],
+  );
+
+  useEffect(() => {
+    return () => {
+      Logger.log('On blank area', { blankAreaTrakerResult }); // lower value means less blank space and better performance, might be less of 150ms
+    };
+  }, []);
+
   return (
     <StyledList
+      ref={ref}
       data={data}
       keyExtractor={(item: unknown, index: number): string => `key${index}`}
-      getItemType={(item: any) => (item.type ? item.type : null)}
+      getItemType={(item: any) => (item.type ? item.type : 'Text')}
       estimatedItemSize={estimatedItemSize || screen_height / data.length}
       horizontal={horizontal}
       keyboardDismissMode="interactive"
       renderItem={renderItem || renderElement}
       showsHorizontalScrollIndicator={showsHorizontalScrollIndicator}
+      onLoad={onLoadListener}
+      onBlankArea={onBlankArea}
     />
   );
 };
